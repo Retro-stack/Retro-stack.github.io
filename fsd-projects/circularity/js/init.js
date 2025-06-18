b0a998104e49d08278e651c1a122754af6af3b30
Retro-stack <83553632+Retro-stack@users.noreply.github.com>
2025-06-18 15:34:02 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -26,14 +26,25 @@ var init = function (window) {
 
 
         // TODO 2 : Create a function that draws a circle 
+         function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+         }
+            
         
 
 
         // TODO 3 : Call the drawCircle() function
+        
 
 
 
         // TODO 7 : Use a loop to create multiple circles
+        for (var i = 0; i < 25; i++) {
+         drawCircle();  
+        }
 
 
 
@@ -49,12 +60,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
            
 
-            // TODO 8 / TODO 9 : Iterate over the array
+            // TODO 8 / TODO 9 : Iterate over the array 
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
            
             
         }
@@ -73,6 +87,7 @@ var init = function (window) {
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
+    
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
